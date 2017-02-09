e3b0290b795cdb904cd710fa6f85d3723d688d13
Pavel Prokofyev <feorand@gmail.com>
2017-02-09 14:45:19 +0500

    added SetScore

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+	public static int Score;
+	public GameObject scoreText;
+
+	public void SetScore(int score) {
+		var uiText = scoreText.GetComponent<Text> ();
+		uiText.text = "Score: " + score.ToString();
+	}
+}
