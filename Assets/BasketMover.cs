072f7bd91b6ffb58ce745d01157778946423c956
Pavel Prokofyev <feorand@gmail.com>
2017-02-10 09:57:05 +0500

    refactored vector reassigning

--- Assets/BasketMover.cs
+++ Assets/BasketMover.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasketMover : MonoBehaviour {
-	
-	// Update is called once per frame
 	void Update () {
 		var screenMousePosition = Input.mousePosition;
 		screenMousePosition.z = Camera.main.transform.position.z;
+
 		var mousePosition = Camera.main.ScreenToWorldPoint(screenMousePosition);
-		var basketPosition = transform.position;
-		basketPosition.x = mousePosition.x;
-		transform.position = basketPosition;
+		var transition = new Vector3 (mousePosition.x - transform.position.x, 0, 0);
+
+		transform.Translate (transition);
 	}
 }
